da0ba4c175c27f60458cf53795ca1685dc85bc11
David919 <114964420+dcsan919@users.noreply.github.com>
2024-07-29 13:35:38 -0600

    mejora de filtros y errores
    final xd

--- src/functions/function.event.js
+++ src/functions/function.event.js
@@ -22,10 +22,11 @@ class Evento{
        }
       
        //filtro por Nombre de evento, Categoría y tipo de evento
-        filtro (req, res) {
-        const { nombre_evento, hora, category, tipo_evento } = req.query;
-        
-        let sql = `SELECT a.evento_id, a.nombre AS nombre_evento, a.fecha_inicio, a.fecha_termino, a.hora, a.ubicacion, a.max_per, a.estado, a.fecha_autorizacion, 
+        async filtro (req, res) {
+          try {
+            const { nombre_evento, hora, category, tipo_evento } = req.query;
+
+            let sql = `SELECT a.evento_id, a.nombre AS nombre_evento, a.fecha_inicio, a.fecha_termino, a.hora, a.ubicacion, a.max_per, a.estado, a.fecha_autorizacion, 
                             b.nombre AS tipo_evento, e.nombre AS organizador_nombre, e.nombre AS autorizado_nombre, c.estado, d.nombre AS categoria_nombre, f.imagen_url 
                      FROM Eventos a 
                      INNER JOIN Tipos_Evento b ON a.tipo_evento_id = b.tipo_evento_id  
@@ -35,12 +36,14 @@ class Evento{
                      INNER JOIN Imagenes f ON a.evento_id = f.evento_id
                      WHERE 1=1 AND c.estado = "APROBADO"`
                      const params = [];
+                     //Cambié la forma de consulta pa que puedan buscar por el primer nombre sin necesidad de que pongan todo el nombre
+                     //filtro?nombre_evento= algo así y te saldrá el evento
                      if (nombre_evento) {
-                      sql += ' AND a.nombre = ?';
-                      params.push(nombre_evento);
+                      sql += ' AND a.nombre LIKE ?';
+                      params.push(`${nombre_evento}%`);
                     }
                     if (hora) {
-                      sql += ' AND a.nombre = ?';
+                      sql += ' AND a.hora = ?';
                       params.push(hora);
                     }
                      if (category) {
@@ -53,26 +56,37 @@ class Evento{
                     } 
                     sql += ' ORDER BY ABS(DATEDIFF(fecha_inicio, CURDATE()))';
 
-        db.query(sql, params, (error, results) => {
-            if (error) {
-                console.error("Error en la consulta", error);
-                return res.status(500).json({ error: "Error en la consulta" });
-            }
-            if (results.length === 0) {
-              let errorMsg = 'No se encontraron eventos con';
-              if (category && tipo_evento) {
-                errorMsg += ` la categoria: ${category} y el tipo de evento: ${tipo_evento}`;
-              } else if (nombre_evento) {
-                errorMsg += ` el nombre': ${nombre_evento}`;
-              } else if (category) {
-                errorMsg += ` la categoria': ${category}`;
-              } else if (tipo_evento) {
-                errorMsg += ` el tipo de evento: ${tipo_evento}`;
+            //await agregado xd
+            const results = await new Promise((resolve, reject)=>{
+              db.query(sql, params, (error, results) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(results);
+                }
+              })
+            })
+
+              if (results.length === 0) {
+                let errorMsg = 'No se encontraron eventos con ';
+
+                const filterMessages = [];
+                if (nombre_evento) filterMessages.push(`el nombre: ${nombre_evento}`);
+                if (hora) filterMessages.push(`la hora: ${hora}`);
+                if (category) filterMessages.push(`la categoría: ${category}`)
+                if (tipo_evento) filterMessages.push(`el tipo de evento: ${tipo_evento}`)
+
+                if (filterMessages.length > 0) {
+                  errorMsg += ' ' + filterMessages.join(', ');
               }
               return res.status(404).json({ error: errorMsg });
-            }
-            res.json(results);
-        });
+              }
+              res.json(results);
+          
+          } catch (error) {
+            console.error({messaje: "Error with filter", error})
+            res.status(500).json({ error: "Error en la consulta" });
+          }
     };
     
 }
